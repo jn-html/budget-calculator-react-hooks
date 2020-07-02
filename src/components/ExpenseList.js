504ae30f42471c167190c091e052ed2df6f2985a
jn-html <marmainjean@gmail.com>
2020-07-02 21:23:30 +0200

    Folder Structure

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Item from './ExpenseItem';
+
+const ExpenseList = () => {
+   return (
+      <div>
+         Expense list
+         <Item />
+      </div>
+   );
+}
+
+export default ExpenseList;
