8dc2f84a5604e26ea4a8100bbc8b65f417930dbc
jn-html <marmainjean@gmail.com>
2020-07-02 21:51:32 +0200

    Initial List

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import ExpenseList from './components/ExpenseList';
 import ExpenseForm from './components/ExpenseForm';
 import Alert from './components/Alert';
+// import uuid from 'uuid/v4'
+import { v4 as uuidv4 } from 'uuid';
+
+const initialExpenses = [
+  {id:uuidv4(), charge: "rent", amount:1600},
+  {id:uuidv4(), charge: "car payment", amount:400},
+  {id:uuidv4(), charge: "recreadit card billnt", amount:1200}
+];
 
 function App() {
   return (
