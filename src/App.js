ecf0114eec1bd3f522912f6c58584c5e115f3bb3
jn-html <marmainjean@gmail.com>
2020-07-07 12:04:02 +0200

    UseState Setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import ExpenseList from './components/ExpenseList';
 import ExpenseForm from './components/ExpenseForm';
@@ -12,7 +12,24 @@ const initialExpenses = [
   {id:uuidv4(), charge: "recreadit card billnt", amount:1200}
 ];
 
+// import useState()
+// function  returns [] with two values
+// the actual value of the state
+// function for updates/control
+// default value
+
+// class based component
+// state = {expenses:initialExpenses}
+// this.setState({})
+
 function App() {
+  // const result = useState(initialExpenses);
+  // const expenses = result[0];
+  // const setExpenses = result[1];
+  // console.log(expenses, setExpenses);
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+
   return (
     <div>
       hello from app
